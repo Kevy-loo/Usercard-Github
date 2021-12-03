ff08a232c41cb680ff57c20a57bb361b9cc99645
kevin-liu <liu.kevin1997@gmail.com>
2021-12-02 19:42:36 -0500

    finished assignment

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,16 @@
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+function getNewCard(username){
+  axios.get(`https://api.github.com/users/${username}`)
+.then(resp => {
+  document.querySelector('.cards').appendChild(newFunction(resp.data));
+}).catch(error => console.error(error));
+
+}
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,7 +36,71 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+followersArray.forEach(username => getNewCard(username));
+
+function newFunction(obj){
+  //creating elements
+  const cardDiv = document.createElement('div');
+  const image = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const userName = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const profileLink = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+  
+
+  console.log(cardDiv)
+
+  cardDiv.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  userName.classList.add('username');
+
+  // cardDiv.appendChild(image);
+  // cardDiv.appendChild(cardInfo);
+  // cardInfo.appendChild(name);
+  // cardInfo.appendChild(userName)
+  // cardInfo.appendChild(location);
+  // cardInfo.appendChild(profile);
+  // profile.appendChild(profileLink);
+  // cardInfo.appendChild(followers);
+  // cardInfo.appendChild(following);
+  // cardInfo.appendChild(bio);
+  
+  // console.log(cardDiv);
+
+  image.src = obj.avatar_url;
+  name.textContent = `Name: ${obj.name}`;
+  userName.textContent = obj.login;
+  location.textContent = `Location ${obj.location}`;
+  profile.textContent = 'Profile';
+  profileLink.textContent = 'Link to profile';
+  profileLink.href = obj.html_url;
+  followers.textContent = `Followers: ${obj.followers}`;
+  following.textContent = `Following: ${obj.following}`;
+  bio.textContent = `bio: ${obj.bio}`;
+  
+  cardDiv.appendChild(image);
+  cardDiv.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(userName)
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(profileLink);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+  
+  return cardDiv;
+
+}
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
